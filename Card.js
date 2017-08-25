ce9ac811178745261fe4b8d9bf51cb6ab6b0bc27
Bartosz Sekula <bartosz.sekula@episerver.com>
2017-08-25 22:50:10 +0200

    Initial commit with DeckOfCards

---
+++ Card.js
@@ -0,0 +1,9 @@
+class Card {
+    //  Summary:
+    //      Represents a single card
+    //  Constructor:
+    //      Card(Color c, Suite s) - constructs a new Card instance
+    //  Public API:
+    //      Color - returns the color of the card
+    //      Suit - returns the suid of the card
+}
---
+++ DeckOfCards.js
@@ -0,0 +1,10 @@
+class DeckOfCards {
+    //  Summary:
+    //      This class represents a deck of cards.
+    //  Constructor:
+    //      DeckOfCards(int n, bool shuffle) - constructs a deck of cards combined from n decks, shuffled or not
+    //  Public API:
+    //      Shuffle() - shuffles the remaining cards in the deck
+    //      Take(int n) - get n cards from the deck
+    //      IsEmpty - are there any cards in the deck?
+}
